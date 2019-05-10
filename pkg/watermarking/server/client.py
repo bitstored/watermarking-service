e399854f21087461f755828829f96ddabd21d749
Diana Bejan <diana.bejan@brainloop.com>
2019-05-11 01:21:32 +0300

    add python implementation

---
+++ pkg/watermarking/server/client.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+from __future__ import print_function
+
+from PIL import Image, ImageDraw
+import logging
+import service_pb2
+import service_pb2_grpc
+import sys
+import grpc
+
+
+def run():
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = service_pb2_grpc.TransformationStub(channel)
+    response = stub.ToImage(service_pb2.ToImageRequest(text='Hello world!'))
+    print("Greeter client received: " + str(response.image))
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
---
+++ pkg/watermarking/server/service.py
@@ -0,0 +1,45 @@
+#! /usr/bin/python
+from PIL import Image, ImageDraw
+
+import service_pb2
+import service_pb2_grpc
+import sys
+import grpc
+from concurrent import futures
+import time
+import io
+
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+class Transformer(service_pb2_grpc.TransformationServicer):
+    def ToImage(self, request, context):
+        text = request.text
+        y = 20
+        x = len(text) * 10
+        image = Image.new('RGBA', (x, y), color=(255, 255, 255, 0))
+        d = ImageDraw.Draw(image)
+        d.text((10, 10), text, fill=(0, 0, 0, 255))
+        image.save('test.png')
+        bar = io.BytesIO()
+        image.save(bar, format=image.format)
+        bar = bar.getvalue()
+        return service_pb2.ToImageResponse(bar, x, y)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    service_pb2_grpc.add_TransformationServicer_to_server(
+        Transformer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    serve()
