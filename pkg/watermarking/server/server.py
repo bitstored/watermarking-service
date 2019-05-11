cbf365e7e0e21a91c198fcd9780c1b58fb464c68
Diana Bejan <diana.bejan@brainloop.com>
2019-05-11 14:16:14 +0300

    change server

--- pkg/watermarking/server/server.py
+++ pkg/watermarking/server/server.py
@@ -12,20 +12,22 @@ import io
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
+LETTER_SIZE = 10
+
 
 class Transformer(service_pb2_grpc.TransformationServicer):
     def ToImage(self, request, context):
         text = request.text
-        y = 20
-        x = len(text) * 10
+        y = LETTER_SIZE+10
+        x = len(text) * LETTER_SIZE
         image = Image.new('RGBA', (x, y), color=(255, 255, 255, 0))
         d = ImageDraw.Draw(image)
-        d.text((10, 10), text, fill=(0, 0, 0, 255))
-        # image.save('test.png')
-        bar = io.BytesIO()
-        image.save(bar, format=image.format)
-        bar = bar.getvalue()
-        return service_pb2.ToImageResponse("", 0, 0)
+        d.text((LETTER_SIZE, LETTER_SIZE), text, fill=(0, 0, 0, 255))
+        image.save('test_data/test.png')
+        bytesarray = io.BytesIO()
+        image.save(bytesarray, format="PNG")
+        bytesarray = bytesarray.getvalue()
+        return service_pb2.ToImageResponse(image=bytesarray, x=x, y=y)
 
 
 def serve():
